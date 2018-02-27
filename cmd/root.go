427bbd6ee67fdfbe61d0cfeb210f22667c78839c
Reiichiro Nakano <reiichiro@istellar.jp>
2018-02-27 11:36:29 +0900

    Use cobra template

---
+++ cmd/root.go
@@ -0,0 +1,88 @@
+/*
+ * Starcoder - a server to read/write data from/to the stars, written in Go.
+ * Copyright (C) 2018 InfoStellar, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "starcoder",
+	Short: "A GRPC server for running GNURadio flowgraphs",
+	Long: `Starcoder is a GRPC server for running GNURadio flowgraphs`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.starcoder-cobra.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".starcoder-cobra" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".starcoder-cobra")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
--- cmd/serve.go
+++ cmd/serve.go
@@ -14,12 +14,12 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
-package main
+package cmd
 
 import (
+	"github.com/spf13/cobra"
 	pb "github.com/infostellarinc/starcoder/api"
 	"golang.org/x/net/context"
 	"net"
@@ -54,23 +54,44 @@ func (s *server) EndProcess(ctx context.Context, in *pb.EndProcessRequest) (*pb.
 	}, nil
 }
 
-func main() {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatalf("failed to get user: %v", err)
-	}
-	lis, err := net.Listen("tcp", bindAddress)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterProcessManagerServer(s, &server{
-		// TODO: Replace this with command line argument and use Packr to default to compiled in flowgraphs
-		flowgraphDir: filepath.Join(usr.HomeDir, ".starcoder/flowgraphs"),
-	})
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start the Starcoder server",
+	Long: `Start the Starcoder server`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Println("serve called")
+		usr, err := user.Current()
+		if err != nil {
+			log.Fatalf("failed to get user: %v", err)
+		}
+		lis, err := net.Listen("tcp", bindAddress)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		s := grpc.NewServer()
+		pb.RegisterProcessManagerServer(s, &server{
+			// TODO: Replace this with command line argument and use Packr to default to compiled in flowgraphs
+			flowgraphDir: filepath.Join(usr.HomeDir, ".starcoder/flowgraphs"),
+		})
+		// Register reflection service on gRPC server.
+		reflection.Register(s)
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
