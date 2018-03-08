fa6f9e9a97d1d09e6f1b3d13ae000b85b4ef88b2
Reiichiro Nakano <reiichiro@istellar.jp>
2018-03-08 16:50:19 +0900

    typo

--- gr-grgrpc/include/grgrpc/msg_sink.h
+++ gr-grgrpc/include/grgrpc/msg_sink.h
@@ -46,7 +46,7 @@ namespace gr {
        * class. grgrpc::msg_sink::make is the public interface for
        * creating new instances.
        */
-      static sptr make(const std::string& addr);
+      static sptr make(const std::string& address);
     };
 
   } // namespace grgrpc
