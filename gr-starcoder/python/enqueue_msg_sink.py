14f8c0e290b76c71da11ff67ea61f2b40a8486c8
Reiichiro Nakano <reiichiro@istellar.jp>
2018-03-12 14:58:45 +0900

    add python enqueue message sink

---
+++ gr-starcoder/python/enqueue_msg_sink.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2018 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+import numpy
+from gnuradio import gr
+import pmt
+from collections import deque
+
+class enqueue_msg_sink(gr.basic_block):
+    """
+    docstring for block enqueue_msg_sink
+    """
+    def __init__(self):
+        gr.basic_block.__init__(self,
+            name="enqueue_msg_sink",
+            in_sig=[],
+            out_sig=[])
+        self.q = deque()
+        self.message_port_register_in(pmt.intern("in"))
+        self.set_msg_handler(pmt.intern("in"), self.msg_handler)
+
+    def get_q(self):
+        return self.q
+
+    def msg_handler(self, pmt):
+        self.q.append(pmt)
+        print("Received pmt", pmt)
