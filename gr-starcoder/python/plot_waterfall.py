cc4ad0d60e0795ce6176f90208e2f563e64fad29
Reiichiro Nakano <reiichiro@istellar.jp>
2018-04-19 16:54:48 +0900

    fix pytohn licenses

--- gr-starcoder/python/plot_waterfall.py
+++ gr-starcoder/python/plot_waterfall.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2018 Infostellar.
+# Copyright 2018 Infostellar. Inc,
 #
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,6 +18,23 @@
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
 #
+#  Copyright (C) 2017, Libre Space Foundation <http://librespacefoundation.org/>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+
 import matplotlib
 matplotlib.use('agg')
 
