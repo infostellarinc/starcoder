c9173874243ef2e3dd5e3a0bcbd42b93800104cb
Reiichiro Nakano <reiichiro@istellar.jp>
2018-05-28 14:29:09 +0900

    working change plot waterfall to oo api

--- gr-starcoder/python/plot_waterfall.py
+++ gr-starcoder/python/plot_waterfall.py
@@ -38,6 +38,9 @@
 import matplotlib
 matplotlib.use('agg')
 
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 import io
 import numpy as np
 import matplotlib.pyplot as plt
@@ -61,7 +64,7 @@ def float_to_str(f):
     return '{:.1}'.format(f)
 
 
-def plot_waterfall(arr, samp_rate, center_freq, rps, fft, filename):
+def old_plot_waterfall(arr, samp_rate, center_freq, rps, fft, filename):
 
     plt.figure(figsize=(12.8, 20.8), dpi=200)
     plt.imshow(arr, cmap=cm.nipy_spectral,
@@ -114,3 +117,68 @@ def plot_waterfall(arr, samp_rate, center_freq, rps, fft, filename):
         plt.savefig(buf, bbox_inches='tight', pad_inches=0.2, format='png')
         plt.close()
         return buf.getvalue()
+
+
+def plot_waterfall(arr, samp_rate, center_freq, rps, fft, filename):
+
+    fig = Figure(figsize=(12.8, 20.8), dpi=200)
+    FigureCanvas(fig)
+
+    ax = fig.add_subplot(111)
+
+    im = ax.imshow(arr, cmap=cm.nipy_spectral,
+               interpolation='none', vmin=int(np.mean(arr)), vmax=arr.max(),
+               alpha=1, aspect='auto')
+    cb = fig.colorbar(im, ax=ax, aspect=50)
+    cb.set_label('Power (dB)')
+
+    cell_freq = samp_rate / 10.0
+    center_tick = fft/2
+    carriers_per_tick = int(cell_freq * fft / samp_rate)
+    nticks = 9
+
+    x_ticks = [center_tick]
+    x_tick_labels = [float_to_str(center_freq)]
+    for i in range(nticks/2):
+        x_ticks.append((i + 1) * carriers_per_tick + center_tick)
+        x_ticks.insert(0, center_tick - (i + 1) * carriers_per_tick)
+        x_tick_labels.append(float_to_str(center_freq + (i + 1) * cell_freq))
+        x_tick_labels.insert(0, float_to_str(center_freq - (i + 1) * cell_freq))
+
+    n_rows_per_tick = 60 * rps
+    y_ticks = []
+    y_tick_labels = []
+    j = 0
+    for i in range(len(arr), 0, -n_rows_per_tick):
+        y_ticks.append(i)
+        y_tick_labels.append(j * 60)
+        j = j + 1
+
+    ax.set_xticks(x_ticks)
+    ax.set_xticklabels(x_tick_labels, size='x-large')
+    if len(y_ticks) > 0:
+        ax.set_yticks(y_ticks)
+        ax.set_yticklabels(y_tick_labels, size='x-large')
+    ax.grid(b='on', linestyle='dashed', linewidth=1,
+            color='#000000', alpha=0.3)
+    minor_locator_y = IndexLocator(10 * rps, len(arr) % (10 * rps))
+    ax.get_yaxis().set_minor_locator(minor_locator_y)
+    ax.get_yaxis().grid(which='minor', color='black', linestyle='-', linewidth=0.05, alpha=0.7)
+    ax.get_xaxis().grid(which='minor', color='black', linestyle='-', linewidth=0.1, alpha=0.6)
+    ax.get_xaxis().set_minor_locator(AutoMinorLocator(10))
+    ax.set_xlabel('Frequency (Hz)')
+    ax.set_ylabel('Time (seconds)')
+
+    if filename != '':
+        fig.savefig(filename, bbox_inches='tight', pad_inches=0.2, format='png')
+
+    with io.BytesIO() as buf:
+        buf = io.BytesIO()
+        fig.savefig(buf, bbox_inches='tight', pad_inches=0.2, format='png')
+        return buf.getvalue()
+
+
+if __name__ == '__main__':
+    arr = np.random.randint(0, 256, [1024, 60*60], dtype=np.uint8)
+    plot_waterfall(arr, 112500., 0., 10, 1024, '/home/rei/bah2.png')
+    old_plot_waterfall(arr, 112500., 0., 10, 1024, '/home/rei/bah.png')
