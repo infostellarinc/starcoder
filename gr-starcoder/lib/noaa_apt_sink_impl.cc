ce32f204261edb301fbffaf121de4489d9661cd3
Reiichiro Nakano <reiichiro@istellar.jp>
2018-06-18 12:49:08 +0900

    remove debug statement

--- gr-starcoder/lib/noaa_apt_sink_impl.cc
+++ gr-starcoder/lib/noaa_apt_sink_impl.cc
@@ -130,7 +130,6 @@ void noaa_apt_sink_impl::write_image(std::string filename) {
     // TODO: Writes out to /tmp since Boost GIL doesn't support writing to streams.
     // This should be fixed moving forward
     boost::filesystem::path temp = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
-    GR_LOG_DEBUG(d_logger, temp.native());
 
     boost::gil::detail::png_writer writer(temp.native().c_str());
 
