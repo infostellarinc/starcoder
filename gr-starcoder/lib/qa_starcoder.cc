a94873cb16050f2aa3fbce34512240da052af061
Reiichiro Nakano <reiichiro@istellar.jp>
2018-07-02 14:42:03 +0900

    empty test

--- gr-starcoder/lib/qa_starcoder.cc
+++ gr-starcoder/lib/qa_starcoder.cc
@@ -34,12 +34,14 @@
 #include <chrono>
 #include <thread>
 #include "qa_enqueue_message_sink.h"
+#include "qa_meteor_decoder.h"
 
 CppUnit::TestSuite *qa_starcoder::suite() {
   CppUnit::TestSuite *s = new CppUnit::TestSuite("starcoder");
   s->addTest(gr::starcoder::qa_enqueue_message_sink::suite());
-  s->addTest(new CppUnit::TestCaller<qa_starcoder>(
-      "run_ar2300_source_block", &qa_starcoder::run_ar2300_source_block));
+  s->addTest(gr::starcoder::qa_meteor_decoder::suite());
+  //s->addTest(new CppUnit::TestCaller<qa_starcoder>(
+  //    "run_ar2300_source_block", &qa_starcoder::run_ar2300_source_block));
 
   return s;
 }
