c8c75066e5eb5b1df7ba8d26834ca8c3a1e179cf
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2018-08-27 18:00:15 +0900

    dB to dBFS (#71)

--- gr-starcoder/python/plot_waterfall.py
+++ gr-starcoder/python/plot_waterfall.py
@@ -72,7 +72,7 @@ def plot_waterfall(arr, samp_rate, center_freq, rps, fft, filename):
                interpolation='none', vmin=int(np.mean(arr)), vmax=arr.max(),
                alpha=1, aspect='auto', origin='lower')
     cb = fig.colorbar(im, ax=ax, aspect=50)
-    cb.set_label('Power (dB)')
+    cb.set_label('Power (dBFS)')
 
     cell_freq = samp_rate / 10.0
     center_tick = fft/2
