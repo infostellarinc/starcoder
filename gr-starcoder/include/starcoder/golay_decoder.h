d8db9a2ab7f2969e1909ab85108619583e1822b3
Kenichi Fukushima <ken.fukushima@gmail.com>
2018-11-26 18:53:55 +0900

    fix copyright header

--- gr-starcoder/include/starcoder/golay_decoder.h
+++ gr-starcoder/include/starcoder/golay_decoder.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2018 Infostellar.
+ * Copyright 2018 Infostellar, Inc.
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
