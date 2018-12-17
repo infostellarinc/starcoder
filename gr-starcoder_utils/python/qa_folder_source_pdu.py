91be4ea7e1ad3d22777cb1db4ce805b8fac4a504
Reiichiro Nakano <reiichiro@istellar.jp>
2018-12-17 19:17:12 +0900

    tests for folder source and update evaluator to use it

--- gr-starcoder_utils/python/qa_folder_source_pdu.py
+++ gr-starcoder_utils/python/qa_folder_source_pdu.py
@@ -21,20 +21,82 @@
 
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
+import numpy as np
+import pmt
 import starcoder_utils_swig as starcoder_utils
+import tempfile
+import os
+import time
+
 
 class qa_folder_source_pdu (gr_unittest.TestCase):
-
     def setUp (self):
         self.tb = gr.top_block ()
+        self.tempdir_path = tempfile.mkdtemp()
+
+        self.tempfiles = []
+        self.bytes1 = os.urandom(259)
+        self.bytes2 = os.urandom(260)
+        for i in range(20):
+            handle, filename = tempfile.mkstemp(dir=self.tempdir_path)
+
+            with open(filename, 'w') as f:
+                f.write(self.bytes1 if i > 9 else self.bytes2)
+            os.close(handle)
+
+            self.tempfiles.append(filename)
 
     def tearDown (self):
         self.tb = None
+        for filename in self.tempfiles:
+            try:
+                os.remove(filename)
+            except OSError:
+                pass
+        try:
+            os.rmdir(self.tempdir_path)
+        except OSError:
+            pass
 
-    def test_001_t (self):
-        # set up fg
-        self.tb.run ()
-        # check data
+    def test_001_regular(self):
+        src = starcoder_utils.folder_source_pdu(self.tempdir_path, 259, 0)
+        snk = blocks.message_debug()
+
+        self.tb.msg_connect((src, 'out'), (snk, 'store'))
+
+        self.tb.start()
+        time.sleep(1)
+        self.tb.stop()
+        self.tb.wait()
+
+        self.assertEqual(snk.num_messages(), 10)
+        for i in range(snk.num_messages()):
+            self.assertTrue(
+                np.array_equal(
+                    np.frombuffer(self.bytes1, dtype=np.uint8),
+                    pmt.to_python(snk.get_message(i))[1]
+                )
+            )
+
+    def test_002_regular(self):
+        src = starcoder_utils.folder_source_pdu(self.tempdir_path, 260, 0)
+        snk = blocks.message_debug()
+
+        self.tb.msg_connect((src, 'out'), (snk, 'store'))
+
+        self.tb.start()
+        time.sleep(1)
+        self.tb.stop()
+        self.tb.wait()
+
+        self.assertEqual(snk.num_messages(), 10)
+        for i in range(snk.num_messages()):
+            self.assertTrue(
+                np.array_equal(
+                    np.frombuffer(self.bytes2, dtype=np.uint8),
+                    pmt.to_python(snk.get_message(i))[1]
+                )
+            )
 
 
 if __name__ == '__main__':
