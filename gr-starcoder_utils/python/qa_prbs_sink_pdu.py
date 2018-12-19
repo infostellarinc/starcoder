04553ae67c033befe5d59b53a38a69de0a4511de
Reiichiro Nakano <reiichiro@istellar.jp>
2018-12-19 16:54:55 +0900

    more tests

--- gr-starcoder_utils/python/qa_prbs_sink_pdu.py
+++ gr-starcoder_utils/python/qa_prbs_sink_pdu.py
@@ -79,7 +79,7 @@ class qa_prbs_sink_pdu (gr_unittest.TestCase):
         self.assertEqual(snk.statistics["Total number of duplicates"], 0)
         self.assertAlmostEqual(snk.statistics["Frame error rate"], 0)
 
-    def test_001_miss_some_packets(self):
+    def test_002_miss_some_packets(self):
         snk = prbs_sink_pdu(self.PACKET_LENGTH_BYTES*8, self.PRBS_RESET_LEN, self.NUM_GENERATED_PACKETS)
 
         # We just need something connected to the trimmer block for
@@ -105,6 +105,126 @@ class qa_prbs_sink_pdu (gr_unittest.TestCase):
         self.assertEqual(snk.statistics["Total number of unique packets after FEC"], 180)
         self.assertEqual(snk.statistics["Total number of duplicates"], 0)
         self.assertAlmostEqual(snk.statistics["Frame error rate"], 0.1)
+
+    def test_003_some_unfixable_packets(self):
+        snk = prbs_sink_pdu(self.PACKET_LENGTH_BYTES*8, self.PRBS_RESET_LEN, self.NUM_GENERATED_PACKETS)
+
+        # We just need something connected to the trimmer block for
+        # the flowgraph to compile, but we'll post messages to it directly
+        src = blocks.message_strobe(pmt.PMT_NIL, 9999999)
+
+        self.tb.msg_connect((src, 'strobe'), (snk, 'all'))
+        self.tb.msg_connect((src, 'strobe'), (snk, 'corrected'))
+
+        self.tb.start()
+        for packet in self.packets[:180]:
+            snk.to_basic_block()._post(pmt.intern('all'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(packet)))
+        for packet in self.packets[:160]:
+            snk.to_basic_block()._post(pmt.intern('corrected'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(packet)))
+        time.sleep(0.1)
+        self.tb.stop()
+        self.tb.wait()
+
+        self.assertEqual(snk.statistics["Expected number of packets sent"], 200)
+        self.assertEqual(snk.statistics["Total number of packets received"], 180)
+        self.assertEqual(snk.statistics["Total number of correct packets after FEC"], 160)
+        self.assertEqual(snk.statistics["Total number of wrong length packets after FEC (This should be 0)"], 0)
+        self.assertEqual(snk.statistics["Total number of erroneous packets after FEC (This should be 0)"], 0)
+        self.assertEqual(snk.statistics["Total number of unique packets after FEC"], 160)
+        self.assertEqual(snk.statistics["Total number of duplicates"], 0)
+        self.assertAlmostEqual(snk.statistics["Frame error rate"], 0.2)
+
+    def test_004_some_erroneous_packets(self):
+        snk = prbs_sink_pdu(self.PACKET_LENGTH_BYTES*8, self.PRBS_RESET_LEN, self.NUM_GENERATED_PACKETS)
+
+        # We just need something connected to the trimmer block for
+        # the flowgraph to compile, but we'll post messages to it directly
+        src = blocks.message_strobe(pmt.PMT_NIL, 9999999)
+
+        self.tb.msg_connect((src, 'strobe'), (snk, 'all'))
+        self.tb.msg_connect((src, 'strobe'), (snk, 'corrected'))
+
+        self.tb.start()
+        for packet in self.packets:
+            snk.to_basic_block()._post(pmt.intern('all'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(packet)))
+        for i, packet in enumerate(self.packets):
+            if i == 10:
+                packet = np.copy(packet)
+                packet[10] = 0x00
+            snk.to_basic_block()._post(pmt.intern('corrected'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(packet)))
+        time.sleep(0.1)
+        self.tb.stop()
+        self.tb.wait()
+
+        self.assertEqual(snk.statistics["Expected number of packets sent"], 200)
+        self.assertEqual(snk.statistics["Total number of packets received"], 200)
+        self.assertEqual(snk.statistics["Total number of correct packets after FEC"], 199)
+        self.assertEqual(snk.statistics["Total number of wrong length packets after FEC (This should be 0)"], 0)
+        self.assertEqual(snk.statistics["Total number of erroneous packets after FEC (This should be 0)"], 1)
+        self.assertEqual(snk.statistics["Total number of unique packets after FEC"], 199)
+        self.assertEqual(snk.statistics["Total number of duplicates"], 0)
+        self.assertAlmostEqual(snk.statistics["Frame error rate"], 1/200.)
+
+    def test_005_some_wrong_length_packets(self):
+        snk = prbs_sink_pdu(self.PACKET_LENGTH_BYTES*8, self.PRBS_RESET_LEN, self.NUM_GENERATED_PACKETS)
+
+        # We just need something connected to the trimmer block for
+        # the flowgraph to compile, but we'll post messages to it directly
+        src = blocks.message_strobe(pmt.PMT_NIL, 9999999)
+
+        self.tb.msg_connect((src, 'strobe'), (snk, 'all'))
+        self.tb.msg_connect((src, 'strobe'), (snk, 'corrected'))
+
+        self.tb.start()
+        for packet in self.packets:
+            snk.to_basic_block()._post(pmt.intern('all'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(packet)))
+        for i, packet in enumerate(self.packets):
+            if i == 10:
+                packet = np.concatenate((packet, np.array([0, 0], dtype=np.uint8)))
+            elif i == 20:
+                packet = packet[:-2]
+            snk.to_basic_block()._post(pmt.intern('corrected'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(packet)))
+        time.sleep(0.1)
+        self.tb.stop()
+        self.tb.wait()
+
+        self.assertEqual(snk.statistics["Expected number of packets sent"], 200)
+        self.assertEqual(snk.statistics["Total number of packets received"], 200)
+        self.assertEqual(snk.statistics["Total number of correct packets after FEC"], 198)
+        self.assertEqual(snk.statistics["Total number of wrong length packets after FEC (This should be 0)"], 2)
+        self.assertEqual(snk.statistics["Total number of erroneous packets after FEC (This should be 0)"], 0)
+        self.assertEqual(snk.statistics["Total number of unique packets after FEC"], 198)
+        self.assertEqual(snk.statistics["Total number of duplicates"], 0)
+        self.assertAlmostEqual(snk.statistics["Frame error rate"], 0.01)
+
+    def test_006_some_dupes(self):
+        snk = prbs_sink_pdu(self.PACKET_LENGTH_BYTES*8, self.PRBS_RESET_LEN, self.NUM_GENERATED_PACKETS)
+
+        # We just need something connected to the trimmer block for
+        # the flowgraph to compile, but we'll post messages to it directly
+        src = blocks.message_strobe(pmt.PMT_NIL, 9999999)
+
+        self.tb.msg_connect((src, 'strobe'), (snk, 'all'))
+        self.tb.msg_connect((src, 'strobe'), (snk, 'corrected'))
+
+        self.tb.start()
+        for packet in self.packets:
+            snk.to_basic_block()._post(pmt.intern('all'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(packet)))
+            snk.to_basic_block()._post(pmt.intern('corrected'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(packet)))
+        snk.to_basic_block()._post(pmt.intern('corrected'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(self.packets[1])))
+
+        time.sleep(0.1)
+        self.tb.stop()
+        self.tb.wait()
+
+        self.assertEqual(snk.statistics["Expected number of packets sent"], 200)
+        self.assertEqual(snk.statistics["Total number of packets received"], 200)
+        self.assertEqual(snk.statistics["Total number of correct packets after FEC"], 201)
+        self.assertEqual(snk.statistics["Total number of wrong length packets after FEC (This should be 0)"], 0)
+        self.assertEqual(snk.statistics["Total number of erroneous packets after FEC (This should be 0)"], 0)
+        self.assertEqual(snk.statistics["Total number of unique packets after FEC"], 200)
+        self.assertEqual(snk.statistics["Total number of duplicates"], 1)
+        self.assertAlmostEqual(snk.statistics["Frame error rate"], 0.00)
 
 
 if __name__ == '__main__':
