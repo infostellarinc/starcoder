6fb9b7e7b46f3614174f7aabd5c23c7e50bf2dc8
rei <reiichiro.s.nakano@gmail.com>
2019-07-08 15:21:23 +0900

    collect and send files via API

--- tools/builder/start_flowgraph.py
+++ tools/builder/start_flowgraph.py
@@ -31,12 +31,28 @@ _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
 
 from gnuradio import gr
 
+import grpc
+from google.auth import jwt as google_auth_jwt
+from google.auth.transport import grpc as google_auth_transport_grpc
+
+from stellarstation.api.v1.groundstation import groundstation_pb2
+from stellarstation.api.v1.groundstation import groundstation_pb2_grpc
+from stellarstation.api.v1 import transport_pb2
+
 parser = argparse.ArgumentParser(description="Script to compile and run a flowgraph")
 
 parser.add_argument("-F", "--flowgraph", help="Location of flowgraph file", required=True)
 parser.add_argument("-C", "--config", help="Location of configuration file", required=True)
 parser.add_argument('-X','--collect', nargs='*', help='List of files to collect and send to the ground station API '
                                                       'at the end of the pass. e.g. Waterfall plots')
+
+# The below arguments are only used when there are files to collect.
+parser.add_argument('-K', '--api-key', help="Location of API key for connecting to the ground station API.")
+parser.add_argument('-R', '--root-cert-path', help="Location of root certificate for connecting to the ground station API.")
+parser.add_argument('-U', '--api-url', help="URL for ground station API")
+parser.add_argument('-G', '--ground-station-id', help="Ground Station ID", default='')
+parser.add_argument('-S', '--stream-tag', help="Stream tag for this plan", default='')
+parser.add_argument('-P', '--plan-id', help="Plan ID", default='')
 
 
 def compile_flowgraph(path):
@@ -52,18 +68,63 @@ def compile_flowgraph(path):
     return os.path.join(temp_dir, os.listdir(temp_dir)[0])
 
 
+def setup_api_client(root_cert_path, api_key, api_url):
+    print('Setting up client to communicate with Ground Station API')
+    if not root_cert_path or not api_key:
+        print('Root certificate path or API key not set. Using insecure channel')
+        channel = grpc.insecure_channel(api_url)
+    else:
+        credentials = google_auth_jwt.Credentials.from_service_account_file(
+            api_key,
+            audience='https://api.stellarstation.com'
+        )
+
+        jwt_creds = google_auth_jwt.OnDemandCredentials.from_signing_credentials(credentials)
+        channel_credential = grpc.ssl_channel_credentials(
+            open(root_cert_path, 'br').read()
+        )
+        channel = google_auth_transport_grpc.secure_authorized_channel(
+            jwt_creds, None, api_url, channel_credential
+        )
+    return groundstation_pb2_grpc.GroundStationServiceStub(channel)
+
+
+def generate_request(files_to_collect, ground_station_id, stream_tag, plan_id):
+    # Send the first request to activate the stream.
+    yield groundstation_pb2.GroundStationStreamRequest(ground_station_id=ground_station_id,
+                                                       stream_tag=stream_tag)
+    for path in files_to_collect:
+        print('Processing {}'.format(path))
+        if os.path.isfile(path):
+            with open(path) as f:
+                stream_request = groundstation_pb2.GroundStationStreamRequest(
+                    ground_station_id=ground_station_id,
+                    stream_tag=stream_tag,
+                    satellite_telemetry=groundstation_pb2.SatelliteTelemetry(
+                        plan_id=plan_id,
+                        telemetry=transport_pb2.Telemetry(
+                            framing=transport_pb2.WATERFALL,
+                            data=f.read()
+                        )
+                    )
+                )
+            yield stream_request
+        else:
+            print("WARNING: {} is not a file".format(path))
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
 
     print("Using flowgraph file: {}".format(args.flowgraph))
     print("Using configuration file: {}".format(args.config))
     print("Files to collect: {}".format(args.collect))
-    print("Reading configuration..")
+    print("Reading flowgraph configuration..")
     with open(args.config) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
         if config is None:
             config = dict()
-    print('Configuration', json.dumps(config, indent=2))
+    print('Flowgraph configuration', json.dumps(config, indent=2))
 
     _, ext = os.path.splitext(args.flowgraph)
     if ext == '.py':
@@ -85,6 +146,10 @@ if __name__ == "__main__":
     print("Running flowgraph. CTRL + C to stop.")
     flowgraph.run()
 
-    # TODO: Collect files before exiting
     if args.collect:
         print("Collecting files: {}".format(args.collect))
+        api_client = setup_api_client(args.root_cert_path, args.api_key, args.api_url)
+        for _ in api_client.OpenGroundStationStream(generate_request(
+                args.collect, args.ground_station_id, args.stream_tag, args.plan_id)):
+            # No need to do anything with responses from the server.
+            continue
